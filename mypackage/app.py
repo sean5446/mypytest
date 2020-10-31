7f0f9f09b8427910c3f49270778f5e9d45baea0e
Sean Maguire <smaguire@nanigans.com>
2020-10-31 17:47:05 -0400

    basic test

---
+++ mypackage/app.py
@@ -0,0 +1,8 @@
+
+
+class MyApp():
+  def do_plus(a, b):
+    return a + b
+
+  def do_minus(a, b):
+    return a - b
---
+++ test/test_app.py
@@ -0,0 +1,13 @@
+
+import unittest
+# from unittest.mock import Mock, patch
+
+from mypackage.app import MyApp
+
+
+class TestApp(unittest.TestCase):
+  def test_do_plus(self):
+    self.assertEqual(MyApp.do_plus(1, 1), 2)
+
+  def test_do_minus(self):
+    self.assertEqual(MyApp.do_minus(1, 1), 0)
